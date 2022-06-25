277aa9130fd86b89ca133743f34e2f25685abc6c
Rashidamosi <shidak360@yahoo.com>
2022-06-25 13:21:39 +0100

    Added more html code

---
+++ Javascript.js
@@ -0,0 +1,16 @@
+//The following code prints my name, height and country on the screen.
+
+
+alert('My Name: Rashida Abdul-rashid'); //This prints out myname :)
+alert('My Height: 1.62M'); //This prints out my present height
+alert('My Country: Nigeria') //This prints out my country Nigeria<3 :>
+
+
+
+// var myName = 'Rashida Abdul-rashid';
+// var myHeight = '1.62M';
+// var myCountry = 'Nigeria.....';
+
+// console.log(myName);
+// console.log(myHeight);
+// console.log(myCountry);
